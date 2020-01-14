ea4daeafd14ab1389db6109d99292b32ecdb383c
jakesIII <jayruma@yahoo.com>
2020-01-14 16:01:11 +0300

    Created a function in the views.py

--- app/main/views.py
+++ app/main/views.py
@@ -119,3 +119,24 @@ def pitch(id):
 
         db.session.add(pitch)
         db.session.commit()
+
+        return redirect("/pitches/{pitch_id}".format(pitch_id = pitch.id))
+
+    coment_form = CommentForm()
+    if comment_form.validate_on_submit():
+        comment = comment_form.text.data
+
+        new_comment = Comment(comment = comment,user_id = current_user,pitch_id = pitch)
+
+        new_comment.save_comment()
+
+    comments = Comment.get_comments(pitch)
+    return render_template('pitch.html', pitch=pitch, comment_form=comment_form, comments=comments, date=posted_date)
+
+@main.route('/user/<uname>/pitches')
+def user_pitches(uname):
+    user = User.query.filter_by(username=uname).first()
+    pitches = Pitch.query.filter_by(user_id = user.id).all()
+    pitches_count = Pitch.count_pitches(uname)
+
+    return render_template("profile/pitches.html", user=user,pitches=pitches,pitches_count=pitches_count)
