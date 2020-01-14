8c59260872a22ce9ffc8753190db2add6803b68f
jakesIII <jayruma@yahoo.com>
2020-01-14 15:49:00 +0300

    Created an update profile function in the app

---
+++ app/main/views.py
@@ -0,0 +1,66 @@
+from flask import render_template,request,redirect,url_for,abort
+from . import main
+from flask_login import login_required,current_user
+import datetime
+
+from ..models import User,Pitch,Comment
+from .. import db
+from .forms import UpdateProfile,PitchForm,CommentForm
+# from .. import db,photos
+
+# views
+@main.route('/')
+def index():
+     '''
+    View root page function that returns the index page and its data
+    '''
+    title = "Welcome to One Minute Perfect Pitch"
+
+    # getting pitches by category
+    pickup_pitches = Pitch.get_pitches("pickup_pitches")
+    interview_pitches = Pitch.get_pitches("interview")
+    product_pitches = Pitch.get_pitches("product")
+    promotion_pitches = Pitch.get_pitches("promotion")
+
+    return render_template('index.html',title = title,pickup = pickup_pitches,interview = interview_pitches,product = product_pitches,promotion = promotion_pitches)
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    pitches_count = Pitch.count_pitches(uname)
+
+    if user is None:
+        abort(404)
+
+    return render_template('profile/profile.html',user = user,pitches = pitches_count)
+
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname = user.username))
+
+    return render_template('profile/update.html',form = form)
+
+@main.route('/pitch/new',methods = ['GET','POST'])
+@login_required
+def new_pitches():
+    pitch_form = PitchForm()
+    if pitch_form.validate_on_submit():
+        title = pitch_form.title.data
+        pitch = pitch_form.txt.data
+        category = pitch_form.carrtegory.data
+
+        # updating pitch instance
